240802ec03ad6a05900992aacbd9aa076a0ad4d6
Noah Emmet <noah@noahemmet.com>
2016-04-23 17:01:12 -0700

    Initial Commit

---
+++ Grid/Grid.h
@@ -0,0 +1,19 @@
+//
+//  Grid.h
+//  Grid
+//
+//  Created by Noah Emmet on 4/23/16.
+//  Copyright © 2016 Sticks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Grid.
+FOUNDATION_EXPORT double GridVersionNumber;
+
+//! Project version string for Grid.
+FOUNDATION_EXPORT const unsigned char GridVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Grid/PublicHeader.h>
+
+
